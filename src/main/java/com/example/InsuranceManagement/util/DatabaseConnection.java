374a7563734f2fc61443c39ae35531b7c18569b3
Himanshu V Chaurasia <2423058@cognizant.com>
2025-07-07 15:26:19 +0530

    database connection created

---
+++ src/main/java/com/example/InsuranceManagement/util/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package com.example.InsuranceManagement.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+	private static String db_url="jdbc:mysql://localhost:3306/insurance_db";
+	private static String db_user="root";
+	private static String db_pswd="root";
+	static Connection con;
+	
+	static {
+		try {
+			con=DriverManager.getConnection(db_url,db_user,db_pswd);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	static Connection getConnection() {
+		return con;
+	}
+
+}
